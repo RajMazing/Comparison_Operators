d5549a309411a6800724425e73116bbacb956dc6
RajMazing <caseyraj92@gmail.com>
2022-09-01 20:45:37 -0500

    boolean example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+            int temperature = 22;
+            boolean isWarm = temperature > 20 && temperature < 30;
+            System.out.println(isWarm);
     }
 }
